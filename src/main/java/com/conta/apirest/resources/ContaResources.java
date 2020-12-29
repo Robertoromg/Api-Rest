ec621ce7391f7112233ad0e80503f6188bccafaf
Robertoromg <robertoro_mg@outlook.com>
2020-12-29 13:28:32 -0300

    adiconando requisioes get, post, put, delete

--- src/main/java/com/conta/apirest/resources/ContaResources.java
+++ src/main/java/com/conta/apirest/resources/ContaResources.java
@@ -2,8 +2,12 @@ package com.conta.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +29,20 @@ public class ContaResources {
 	@GetMapping("/contas/{id}")
 	public Conta listaContaUnica(@PathVariable(value="id")long id){
 		return contaRepository.findById(id);
+	}
+	
+	@PostMapping("/contas")
+	public Conta salvaConta(@RequestBody Conta conta) {
+		return contaRepository.save(conta);
+	}
+	
+	@DeleteMapping("/contas")
+	public void deletaConta(@RequestBody Conta conta) {
+		contaRepository.delete(conta);
+	}
+	
+	@PutMapping("/contas")
+	public Conta atualizaConta(@RequestBody Conta conta) {
+		return contaRepository.save(conta);
 	}
 }
